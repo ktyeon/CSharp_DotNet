b4faf7d4e74c900fd448a2a51e3591923fa9023d
ktyeon <qazppo@naver.com>
2023-10-10 14:22:44 -0400

    First practice C# and Dotnet

---
+++ Exercise Files/Start/Overview/Comments/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /// XML Comments are used to help provide documentation
+        /// They start with triple-slashes and have a special syntax 
+                ///<summary>
+                /// XML Comments are used to help provide documentation
+                /// </summary>
+                /// <param name="args">An array of string arguments from the command line</param>        
+                /// <returns>
+                /// No return value
+                /// </returns>
+        static void Main(string[] args)
+        {
+            // Sigle line = comments start with two slashes
+            // you can have as many of these as you want
+            Console.WriteLine("Hello World!");
+            /* Multiple line commets start with a slash adn a star
+            annd can continew for several lines
+            until a closing star and slash are encountered */
+            
+        }
+    }
+}
---
+++ Exercise Files/Start/Overview/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// using System;
+
+// namespace HellowWorld {
+
+//     class Program{
+//         static void Main(Strint[] args){
+
+//                 Console.WriteLine("Hello, World!");
+                
+//                 Console.WriteLine("What is your name?");
+                
+//                 string str = console.ReadLine();
+
+//                 Console.WriteLine("Why, hello there" + str );
+
+//         }
+
+//     }
+// }
+
+
+Console.WriteLine("Hello, World!");            
+Console.WriteLine("What is your name?");           
+string str = console.ReadLine();
+Console.WriteLine("Why, hello there" + str );
