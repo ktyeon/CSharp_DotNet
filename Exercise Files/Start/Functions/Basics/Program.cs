ebad4e5416ee7cdd9202f34145c275c9c8c9a3bb
ktyeon <qazppo@naver.com>
2023-10-15 15:16:00 -0400

    Third Practice in C# and Dotnet

--- Exercise Files/Start/Functions/Basics/Program.cs
+++ Exercise Files/Start/Functions/Basics/Program.cs
@@ -4,13 +4,28 @@
 // unit that can be customized with parameters.
 
 // TODO: Functions have a return type, name, and optional parameters
+float MilesToKm(float miles) {
+    
+    float result = miles * 1.6f;
+   
+    return result;
 
+}
 
 // TODO: A function with no return value has a 'void' type
+void PrintWithPrefix(string thestr) {
+    
+    Console.WriteLine($"::> {thestr}");
+
+}
 
 
 // TODO: Call first function
+Console.WriteLine($"The result is {MilesToKm(8.0f)}");
 
+Console.WriteLine($"The result is {MilesToKm(52.0f)}");
 
 // TODO: Call second function
+PrintWithPrefix("Test string");
 
+PrintWithPrefix("Another test string");
--- Exercise Files/Start/Functions/DefaultParams/Program.cs
+++ Exercise Files/Start/Functions/DefaultParams/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 
 // TODO: Functions can provide default values for their parameters
-void PrintWithPrefix(string thestr, string prefix)
+void PrintWithPrefix(string thestr, string prefix="")
 {
     Console.WriteLine($"{prefix} {thestr}");
 }
 
 // TODO: Test the default parameters
+PrintWithPrefix("Hello there!");
 
+PrintWithPrefix("Hello there!", ">: ");
 
 // TODO: Call with named params
-
+PrintWithPrefix(prefix: "% ", thestr: "Hello there!");
